82ccc5639444e92655406698365147fb074889d2
grzesiek1201 <138705337+grzesiek1201@users.noreply.github.com>
2023-10-28 15:45:39 +0200

    Update test_client.py

--- Client Server app v2/tests/test_client.py
+++ Client Server app v2/tests/test_client.py
@@ -29,37 +29,31 @@ class TestClientServerInteraction(unittest.TestCase):
         client = Client()
         client.connect_to_server('localhost', 8888)
 
-        # Test sending a 'help' command from the client to the server
         print("Test 2 - Sending a 'help' command")
         command = "help"
         client.send_command(command)
         mock_socket.return_value.sendall.assert_called_once_with(command.encode())
 
-        # Simulating the server response for 'help' command
         print("Test 3 - Simulating server response for 'help' command")
         mock_socket.return_value.recv.return_value = b'Help response'
         response = client.receive_data()
         self.assertEqual(response, "Help response")
 
-        # Test sending an 'uptime' command from the client to the server
         print("Test 4 - Sending an 'uptime' command")
         command = "uptime"
         client.send_command(command)
         mock_socket.return_value.sendall.assert_called_with(command.encode())
 
-        # Simulating the server response for 'uptime' command
         print("Test 5 - Simulating server response for 'uptime' command")
         mock_socket.return_value.recv.return_value = b'Uptime response'
         response = client.receive_data()
         self.assertEqual(response, "Uptime response")
 
-        # Test sending an 'info' command from the client to the server
         print("Test 6 - Sending an 'info' command")
         command = "info"
         client.send_command(command)
         mock_socket.return_value.sendall.assert_called_with(command.encode())
 
-        # Simulating the server response for 'info' command
         print("Test 7 - Simulating server response for 'info' command")
         mock_socket.return_value.recv.return_value = b'Info response'
         response = client.receive_data()
