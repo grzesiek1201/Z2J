c056eaf72dfa5cfe3b0202115bc0804cdf391c46
grzesiek1201 <138705337+grzesiek1201@users.noreply.github.com>
2024-01-25 18:26:23 +0100

    Update sql_table.py

--- ServerClientApp/sql_table.py
+++ ServerClientApp/sql_table.py
@@ -5,14 +5,20 @@ class DbBase:
     def __init__(self):
         self.connection = None
         self.connected = False
-
-    def __enter__(self):
         self.DB_HOST = "localhost"
         self.DB_DATABASE = "postgres"
         self.DB_USER = "postgres"
         self.DB_PASSWORD = "postgres"
         self.DB_PORT = 5433
 
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+
+    def connect(self):
         if not self.connected or (self.connection and self.connection.closed):
             try:
                 self.connection = psycopg2.connect(
@@ -29,34 +35,40 @@ class DbBase:
             except (Exception, Error) as error:
                 print("Error while connecting to PostgreSQL:", error)
 
-        return self.connection
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self.connected and self.connection:
-            print("Before closing PostgreSQL connection:", self.connection)
-            self.connection.close()
-            self.connected = False
-            print("PostgreSQL connection is closed")
+    def disconnect(self):
+        try:
+            if self.connected and self.connection:
+                print("Before closing PostgreSQL connection:", self.connection)
+                # ... additional cleanup if needed ...
+        finally:
+            if self.connection is not None:
+                self.connection.close()
+                print("PostgreSQL connection is closed.")
+                self.connected = False
 
     def execute_sql(self, sql, params=None):
+        cursor = self.connection.cursor()
         try:
-            with self.connection.cursor() as cursor:
-                cursor.execute(sql, params)
-                self.connection.commit()
+            cursor.execute(sql, params)
+            self.connection.commit()
         except (Exception, Error) as error:
             print(f"Error executing SQL query: {error}")
+            print(f"SQL Query: {sql}")
+            print(f"Params: {params}")
+            raise
+        finally:
+            cursor.close()
 
     def db_server_start(self):
         try:
-            connection = self.connection
-            if not connection or connection.closed:
-                raise Exception("Connection not established or closed.")
+            self.connect()
 
             print("Connected to UsersBase - DB")
 
-            print("Before executing SQL queries:", connection)
+            print("Before executing SQL queries:", self.connection)
             create_table_users_base = '''
                 CREATE TABLE IF NOT EXISTS UsersBase (
+                    id SERIAL PRIMARY KEY,
                     username VARCHAR(255) NOT NULL UNIQUE,
                     password VARCHAR(255) NOT NULL,
                     role VARCHAR(255) NOT NULL DEFAULT 'USER'
@@ -72,147 +84,110 @@ class DbBase:
                 );
             '''
             self.execute_sql(create_table_users_messages)
-
-            print("After executing SQL queries:", connection)
+            print("After executing SQL queries:", self.connection)
 
         except (Exception, Error) as error:
             print("Error while connecting to PostgreSQL:", error)
+        finally:
+            pass
 
     def register_user(self, username, password):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("INSERT INTO UsersBase (username, password) VALUES (%s, %s)", (username, password))
-            connection.commit()
-        except Exception as e:
-            connection.rollback()
+            sql = "INSERT INTO UsersBase (username, password) VALUES (%s, %s)"
+            params = (username, password)
+            self.execute_sql(sql, params)
+        except psycopg2.Error as e:
+            self.connection.rollback()
             raise e
-        finally:
-            cursor.close()
 
     def authenticate_user(self, username, password):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("SELECT role FROM UsersBase WHERE username = %s AND password = %s", (username, password))
-            role = cursor.fetchone()
-            return role[0] if role else None
-        except Exception as e:
-            raise e
-        finally:
-            cursor.close()
+            with psycopg2.connect(
+                    host=self.DB_HOST,
+                    user=self.DB_USER,
+                    password=self.DB_PASSWORD,
+                    dbname=self.DB_DATABASE,
+                    port=self.DB_PORT,
+                    sslmode='disable'
+            ) as connection:
+                connection.autocommit = True
+                sql = "SELECT role FROM UsersBase WHERE username = %s AND password = %s"
+                params = (username, password)
+                with connection.cursor() as cursor:
+                    cursor.execute(sql, params)
+                    role = cursor.fetchone()
+                    print(f"SQL Query: {cursor.query}")
+                    print(f"Params: {params}")
+                    print(f"Retrieved Role: {role}")
+                    return role[0] if role else None
+        except psycopg2.Error as e:
+            print(f"Error during authentication: {str(e)}")
+            return None
 
     def get_user_role(self, username):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("SELECT role FROM UsersBase WHERE username = %s", (username,))
-            role = cursor.fetchone()
-            return role[0] if role else None
-        except Exception as e:
+            sql = "SELECT role FROM UsersBase WHERE username = %s"
+            params = (username,)
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, params)
+                role = cursor.fetchone()
+                return role[0] if role else None
+        except psycopg2.Error as e:
             raise e
-        finally:
-            cursor.close()
-
-    def register_user(self, username, password):
-        connection = self.connection
-        cursor = connection.cursor()
-        try:
-            cursor.execute("INSERT INTO UsersBase (username, password) VALUES (%s, %s)", (username, password))
-            connection.commit()
-        except Exception as e:
-            connection.rollback()
-            raise e
-        finally:
-            cursor.close()
-
-    def authenticate_user(self, username, password):
-        connection = self.connection
-        cursor = connection.cursor()
-        try:
-            cursor.execute("SELECT role FROM UsersBase WHERE username = %s AND password = %s", (username, password))
-            role = cursor.fetchone()
-            return role[0] if role else None
-        except Exception as e:
-            raise e
-        finally:
-            cursor.close()
-
-    def get_user_role(self, username):
-        connection = self.connection
-        cursor = connection.cursor()
-        try:
-            cursor.execute("SELECT role FROM UsersBase WHERE username = %s", (username,))
-            role = cursor.fetchone()
-            return role[0] if role else None
-        except Exception as e:
-            raise e
-        finally:
-            cursor.close()
 
     def delete_user(self, username):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("DELETE FROM UsersBase WHERE username = %s", (username,))
-            connection.commit()
+            sql = "DELETE FROM UsersBase WHERE username = %s"
+            params = (username,)
+            self.execute_sql(sql, params)
         except psycopg2.Error as e:
-            connection.rollback()
+            self.connection.rollback()
             raise e
-        finally:
-            cursor.close()
 
     def send_message(self, sender, recipient, message):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("INSERT INTO UsersMessages (sender, recipient, message) VALUES (%s, %s, %s)",
-                           (sender, recipient, message))
-            connection.commit()
+            sql = "INSERT INTO UsersMessages (sender, recipient, message) VALUES (%s, %s, %s)"
+            params = (sender, recipient, message)
+            self.execute_sql(sql, params)
             return "Message sent."
         except psycopg2.Error as e:
-            connection.rollback()
-            raise e
-        finally:
-            cursor.close()
+            print(f"Error sending message: {str(e)}")
+            return f"Error sending message: {str(e)}"
 
     def read_messages(self, username):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("SELECT sender, message FROM UsersMessages WHERE recipient = %s", (username,))
+            sql = "SELECT sender, message FROM UsersMessages WHERE recipient = %s"
+            params = (username,)
+            cursor = self.connection.cursor()
+            cursor.execute(sql, params)
             messages = cursor.fetchall()
+            cursor.close()
             return messages
         except psycopg2.Error as e:
-            raise e
-        finally:
-            cursor.close()
+            print(f"Error reading messages: {str(e)}")
+            return f"Error reading messages: {str(e)}"
 
     def show_all_messages(self, recipient):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("SELECT sender, message FROM UsersMessages WHERE recipient = %s", (recipient,))
-            messages = cursor.fetchall()
-            return messages
+            sql = "SELECT sender, message FROM UsersMessages WHERE recipient = %s"
+            params = (recipient,)
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, params)
+                messages = cursor.fetchall()
+                return messages
         except psycopg2.Error as e:
             raise e
-        finally:
-            cursor.close()
 
     def show_all_users(self):
-        connection = self.connection
-        cursor = connection.cursor()
         try:
-            cursor.execute("SELECT username, role FROM UsersBase")
-            users = cursor.fetchall()
-            return users
+            sql = "SELECT username, role FROM UsersBase"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql)
+                users = cursor.fetchall()
+                return users
         except psycopg2.Error as e:
             raise e
-        finally:
-            cursor.close()
 
     def db_server_close(self):
-        connection = self.connection
-        connection.close()
+        self.disconnect()
         print('Server DB - CLOSED')
